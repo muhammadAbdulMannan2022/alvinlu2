fddae48e5d6117a7ec1ca77e7fe169d0191aab4a
abdul mannan <muhammadabdulmannan21@gmail.com>
2025-10-03 18:28:23 +0600

    fixing

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Redirect } from "expo-router";
 
 export default function index() {
-  return <Redirect href="/(auth)" />;
+  // return <Redirect href="/(auth)" />;
+  return <Redirect href="/(tabs)/Home" />;
 }
